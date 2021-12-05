c12efcd4a86deb56d98c673605a1fdb16c69e2a6
Adithya <adi5ver@gmail.com>
2021-12-05 01:24:27 -0500

    Abt.cpp final

--- ankitdiw/src/abt.cpp
+++ ankitdiw/src/abt.cpp
@@ -1,8 +1,8 @@
+   
 #include "../include/simulator.h"
 
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
-
    This code should be used for PA2, unidirectional data transfer 
    protocols (from A to B). Network properties:
    - one way network delay averages five time units (longer if there
@@ -15,129 +15,125 @@
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
-#include<vector>
-#include<iostream>
-#include<stdio.h>
-#include<string.h>
+#include <iostream>
+#include <stdio.h>
+#include <queue>
+#include <string.h>
 
 using namespace std;
-
-std::vector<msg> msgBufferA;
-std::vector<pkt> pktBufferB;
-pkt currentPacket;
+#define Time_out 20.0
 #define FALSE 0
 #define TRUE 1
 
-int nextSeqNoA = 0;
-int currentWaitingAckNo = 0;
-int isReadyToSend = 0;
-int nextSeqNoB = 0;
-int currentAckNoB = 0;
+std::queue<std::string> msgBuffer;
 
-void send_Pkt(int seqNo,int ackNo,char* data,int flag)
+struct Sender
 {
-    //pkt* newPkt = new pkt();
-    cout<<"Entering send_Pkt"<<endl;
-    pkt newPkt;
-    newPkt.seqnum = seqNo;
-    newPkt.acknum = ackNo;
-    strcpy(newPkt.payload,data);
-    newPkt.checksum = 0;
-    cout<<"Entering tolayer3"<<endl;
-    tolayer3(flag,newPkt);
-    cout<<"Leaving tolayer3"<<endl;
-    cout<<"Sent packet with seq no:"<<seqNo<<" data:";
+    int nextSeqNo;
+    int nextAckNo;
+    int isReady;
+}aInfo;
+
+struct Receiver
+{
+    int currAckNo;
+}bInfo;
+
+int compute_checksum(struct pkt *packet)
+{   
+    cout<<"Entering checksum"<<endl;
+    int checksum = 0;
+    checksum += packet->seqnum;
+    checksum += packet->acknum;
     for(int i=0;i<20;++i)
-    cout<<data[i];
-    cout<<"\n";
-    starttimer(0,22.0);
-    cout<<"Leaving send_pkt"<<endl;
-    //delete(newPkt);
+    {
+        checksum += packet->payload[i]; 
+    }
+    cout<<"Leaving checksum"<<endl;
+    return ~checksum;
 }
 
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(struct msg message)
-{
-    cout<<"Received a msg from layer 5 to be sent"<<endl;
-    msgBufferA.push_back(message);
-    if(isReadyToSend)
-    {   
-        cout<<"Sending a existing msg from the buffer"<<endl;
-        send_Pkt(nextSeqNoA,nextSeqNoA,msgBufferA.front().data,0);
-        currentWaitingAckNo = nextSeqNoA;
-        nextSeqNoA = (nextSeqNoA + 1) % 2;
-        isReadyToSend = FALSE;
-        //starttimer(0,100.0);
+{   
+    msgBuffer.push(message.data);
+    if(aInfo.isReady == FALSE)
+    {
+        cout<<"Waiting for acknowledgement of the correct packet, hence adding to message buffer"<<endl;
+        return;
     }
     else
     {
-        cout<<"Buffer the msg as we are waiting for acknowledgement of the existing packet"<<endl;
+        cout<<"Sending the current message"<<endl;
+        pkt packet;
+        packet.seqnum = aInfo.nextSeqNo;
+        packet.acknum = aInfo.nextAckNo;
+        memcpy(packet.payload,message.data,sizeof(message.data));
+        packet.checksum = compute_checksum(&packet);
+        cout<<"Sending from A:\n"<<"msg:"<<message.data<<"\n"<<"Seq Number:"<<aInfo.nextSeqNo<<"\nAck number:"<<aInfo.nextAckNo<<endl;
+        tolayer3(0,packet);
+        aInfo.isReady = FALSE;
+        starttimer(0,Time_out);
     }
-
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(struct pkt packet)
 {
-    cout<<"Received a new packet from B to A with ack_no"<<packet.acknum<<endl;
-    if(packet.acknum == currentWaitingAckNo)
+    cout<<"Ack packet received from B with ack number:"<<packet.acknum<<endl;
+    if((packet.checksum != compute_checksum(&packet)) || (packet.acknum != aInfo.nextAckNo))
     {
-        cout<<"Received the ack for the right packet!!"<<endl;
-        //tolayer5(0,packet.payload);
-        cout<<"Size of vector before erase"<<msgBufferA.size()<<endl;
-        msgBufferA.erase(msgBufferA.begin());
-        cout<<"Size of vector after erase"<<msgBufferA.size()<<endl;
-        stoptimer(0);
-        isReadyToSend = TRUE;
-        if(msgBufferA.size() > 0)
-        {
-            cout<<"Sending a existing msg from the buffer with data:"<<endl;
-            for(int i =0;i<20;++i)
-            cout<<msgBufferA.front().data[i];
-            cout<<endl;
-            send_Pkt(nextSeqNoA,nextSeqNoA,msgBufferA.front().data,0);
-            currentWaitingAckNo = nextSeqNoA;
-            nextSeqNoA = (nextSeqNoA + 1) % 2;
-            isReadyToSend = FALSE;
-            //starttimer(0,100.0);
-        }
+        cout<<"Packet is corrupted/has wrong ackno, waiting ack no is: "<<aInfo.nextAckNo<<endl;
+        return;
     }
-    else
-    {
-        cout<<"Received the wrong acknowlegdement msg, hence resending the current pkt"<<endl;
-        send_Pkt(currentWaitingAckNo,currentWaitingAckNo,msgBufferA.front().data,0);
-        //currentWaitingAckNo = nextSeqNoA;
-        nextSeqNoA = (currentWaitingAckNo + 1) % 2;
-        isReadyToSend = FALSE;
-        //starttimer(0,100.0);
+
+    cout<<"Right acknowledgement received without corruption and right ack no:"<<aInfo.nextAckNo<<endl;
+    stoptimer(0); 
+    msgBuffer.pop();
+    aInfo.isReady = TRUE;
+    aInfo.nextSeqNo = (aInfo.nextSeqNo + 1) % 2;
+    aInfo.nextAckNo = (aInfo.nextAckNo + 1) % 2;
+    if(msgBuffer.size() > 0)
+    {   
+        cout<<"Messages has been queued in Buffer, sending that now"<<endl;
+        pkt packet;
+        packet.seqnum = aInfo.nextSeqNo;
+        packet.acknum = aInfo.nextAckNo;
+        string msg = msgBuffer.front();
+        strncpy(packet.payload,msg.c_str(),20);
+        packet.checksum = compute_checksum(&packet);
+        cout<<"Sending from A:\n"<<"msg:"<<msg<<"\n"<<"Seq Number:"<<aInfo.nextSeqNo<<"\nAck number:"<<aInfo.nextAckNo<<endl;
+        tolayer3(0,packet);
+        aInfo.isReady = FALSE;
+        starttimer(0,Time_out);
     }
+    cout<<"Leaving A_input"<<endl;
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
-{   
-    stoptimer(0);
-    cout<<"Entering Timer Interrupt"<<endl;
-    cout<<"Timer expired for the packet with seq number:"<<currentWaitingAckNo<<endl;
-    cout<<"Resending the packet back to B"<<endl;
-    send_Pkt(currentWaitingAckNo,currentWaitingAckNo,msgBufferA.front().data,0);
-    cout <<"nextSeqno="<<nextSeqNoA<<endl;
-    nextSeqNoA = (currentWaitingAckNo + 1) % 2;
-    cout <<"nextSeqno="<<nextSeqNoA<<endl;
-    //currentWaitingAckNo = newPkt.seqnum;
-    isReadyToSend = FALSE;
-    //starttimer(0,100.0);
-    cout<<"Leaving Timer Interrupt"<<endl;
+{
+    cout<<"Timer has expired for the current packet with Seq number:"<<aInfo.nextSeqNo<<" Hence resending the packet"<<endl;
+    pkt packet;
+    packet.seqnum = aInfo.nextSeqNo;
+    packet.acknum = aInfo.nextAckNo;
+    string msg = msgBuffer.front();
+    strncpy(packet.payload,msg.c_str(),20);
+    packet.checksum = compute_checksum(&packet);
+    cout<<"Sending from A:\n"<<"msg:"<<packet.payload<<"\n"<<"Seq Number:"<<aInfo.nextSeqNo<<"\nAck number:"<<aInfo.nextAckNo<<endl;
+    tolayer3(0,packet);
+    aInfo.isReady = FALSE;
+    starttimer(0,Time_out);
+    cout<<"Leaving timerinterrupt"<<endl;
 }  
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
-  nextSeqNoA = 0;
-  currentWaitingAckNo = 0;
-  msgBufferA.clear();
-  isReadyToSend = TRUE;
+    aInfo.nextSeqNo = 0;
+    aInfo.nextAckNo = 0;
+    aInfo.isReady = TRUE;
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -145,33 +141,38 @@ void A_init()
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
-    cout<<"Packet recieved at B"<<endl;
-    pkt ackPkt;
-    memset(&ackPkt,0,sizeof(pkt));
-    if(packet.seqnum != nextSeqNoB)
+    //3 cases:
+    //case 1: perfect pkt, not received before, transmit ack and call tolayer5()
+    //case 2: perfect pkt, but received before, transmit ack but don't deliver to upper layer
+    if (packet.checksum == compute_checksum(&packet)) 
     {
-        ackPkt.seqnum = nextSeqNoB;
-        ackPkt.acknum = nextSeqNoB;
-        ackPkt.checksum = 0;
-        tolayer3(1,ackPkt);
-    }
-    else
+        cout << "Received packet with payload " << packet.payload << ", seq=" << packet.seqnum << " ack=" << packet.acknum << endl;
+        pkt sendPacket;
+        if (packet.seqnum == bInfo.currAckNo) 
+        {
+            tolayer5(1, packet.payload);
+            sendPacket.acknum = packet.seqnum;
+            sendPacket.checksum = compute_checksum(&sendPacket);
+            tolayer3(1, sendPacket);
+            bInfo.currAckNo = (bInfo.currAckNo + 1) % 2;
+            cout << "Send the ack packet: " << "ack=" << sendPacket.acknum << " next expected seq number:" << bInfo.currAckNo << endl;
+        } 
+        else {
+            sendPacket.acknum = packet.seqnum;
+            sendPacket.checksum = compute_checksum(&sendPacket);
+            cout << "Response to sender: " << "ack=" << sendPacket.acknum << " next expected seq=" << bInfo.currAckNo << endl;
+            tolayer3(1, sendPacket);
+        }
+    } 
+    else 
     {
-        ackPkt.seqnum = nextSeqNoB;
-        ackPkt.acknum = packet.seqnum;
-        tolayer5(1,packet.payload);
-        tolayer3(1,ackPkt);
-        currentAckNoB = packet.seqnum;
-        nextSeqNoB = (nextSeqNoB + 1) % 2;
-
+        cout << "Packet received is corrupted!!!!!" << endl;
     }
-
 }
 
 /* the following rouytine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
-  //NextSeqNoB = 0;
-  currentAckNoB = 0;
+    bInfo.currAckNo = 0;
 }
